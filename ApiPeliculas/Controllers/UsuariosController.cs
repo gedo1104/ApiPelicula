f9a19f6464da9fea4f4923e905541da648bc92a1
Gerardo Ramírez <gedrix.rm93@gmail.com>
2024-01-11 10:36:46 -0500

    2da parte token

--- ApiPeliculas/Controllers/UsuariosController.cs
+++ ApiPeliculas/Controllers/UsuariosController.cs
@@ -1,11 +1,12 @@
-﻿using ApiPeliculas.Repositorio.IRepositorio;
+﻿using ApiPeliculas.Modelos.Dtos;
+using ApiPeliculas.Repositorio.IRepositorio;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiPeliculas.Controllers
 {
-    [Route("api/[usuarios]")]
+    [Route("api/usuarios")]
     [ApiController]
     public class UsuariosController : ControllerBase
     {
@@ -17,6 +18,24 @@ namespace ApiPeliculas.Controllers
         {
             _usRepo = usRepo;
             _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public IActionResult GetUsuarios()
+        {
+            var listaUsuarios = _usRepo.GetUsuarios();
+
+            var listaUsuarioDto = new List<UsuarioLoginDto>();
+
+            foreach (var lista in listaUsuarios)
+            {
+                listaUsuarioDto.Add(_mapper.Map<UsuarioLoginDto>(lista));
+            }
+
+            return Ok(listaUsuarioDto);
         }
     }
 }
